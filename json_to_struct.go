b8afeb9ae24dea6b827287b73ddfd400d5c347dd
nghiahsgs <nghiahsgs@gmail.com>
2021-01-21 11:24:59 +0700

    Create json_to_struct.go

---
+++ json_to_struct.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"strconv"
+)
+
+type User struct {
+	Name string `json:"name"`
+	Age  int    `json:"age"`
+}
+
+func (user *User) greeting() string {
+	return "user name: " + user.Name + " and user age:" + strconv.Itoa(user.Age)
+}
+
+func main() {
+	// parse user
+	// userStr := `{"name":"nghia","age":20}`
+	// var user User
+	// err := json.Unmarshal([]byte(userStr), &user)
+	// if err != nil {
+	// 	fmt.Println(err)
+	// }
+	// fmt.Println(user)
+
+	// parse list user
+	listUserStr := `[{"name":"nghia","age":23},{"name":"nguyen","age":25}]`
+	var listUser []User
+	err := json.Unmarshal([]byte(listUserStr), &listUser)
+	if err != nil {
+		fmt.Println(err)
+	}
+	for _, user := range listUser {
+		fmt.Println(user.greeting())
+	}
+
+}
