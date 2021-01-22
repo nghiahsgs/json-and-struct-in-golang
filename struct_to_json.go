9eec6f783d401b861b373385112ab4556658073f
Khanh Nguyen <khanh.nguyen@shopee.com>
2021-01-22 14:49:36 +0800

    good practice, fixed size slice should be created this way.

--- struct_to_json.go
+++ struct_to_json.go
@@ -5,32 +5,43 @@ import (
 	"fmt"
 )
 
+// Book : book
 type Book struct {
 	Name   string `json:"name"`
 	Year   int    `json:"year"`
 	Author Author `json:"author"`
 }
+
+// Author : author
 type Author struct {
 	Name string `json:"name"`
 	Age  int    `json:"age"`
 }
 
 func main() {
-	var listBook []Book
-	var author1 = Author{Name: "nghiahsgs", Age: 20}
-
-	book1 := Book{Name: "day la ten cuon sach 1", Year: 2015, Author: author1}
-	listBook = append(listBook, book1)
-
-	book2 := Book{Name: "day la ten cuon sach 2", Year: 2016, Author: author1}
-	listBook = append(listBook, book2)
+	var author1 = Author{
+		Name: "nghiahsgs",
+		Age:  20,
+	}
+	listBook := []Book{
+		Book{
+			Name:   "day la ten cuon sach 1",
+			Year:   2015,
+			Author: author1,
+		},
+		Book{
+			Name:   "day la ten cuon sach 2",
+			Year:   2016,
+			Author: author1,
+		},
+	}
 
 	// fmt.Println(listBook)
 
 	// listBookStr, err := json.Marshal(listBook)
-	listBookStr, err := json.MarshalIndent(listBook, "", "    ")
-	if err != nil {
+	if listBookStr, err := json.MarshalIndent(listBook, "", "    "); err != nil {
 		fmt.Println(err)
+	} else {
+		fmt.Println(string(listBookStr))
 	}
-	fmt.Println(string(listBookStr))
 }
